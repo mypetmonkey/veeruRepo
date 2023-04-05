c7e2bae6739eae0145389fe3a84d498b5a1ac50b
rajha <rajha@rajhansji>
2023-04-05 13:15:10 +0530

    ok

---
+++ src/test/java/veeru/testScript_01.java
@@ -0,0 +1,16 @@
+package veeru;
+
+import org.testng.annotations.Test;
+
+public class testScript_01 {
+	@Test
+	public void demo() {
+		System.out.println(" veeru six king");
+		
+	}
+	@Test
+	public void Mango() {
+		System.out.println("veeru fav fruit");
+	}
+
+}
